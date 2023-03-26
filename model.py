89426a9422dd4eecc1b429590f7a57e676b30307
Dzimiela <casey.dzimiela@gmail.com>
2023-03-25 19:09:34 -0500

    Everything passes Chapt 1

--- model.py
+++ model.py
@@ -50,11 +50,6 @@ class Batch:
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
 
-    def allocate(line: OrderLine, batches: List[Batch]) -> str:
-        batch = next(b for b in sorted(batches) if b.can_allocate(line))
-        batch.allocate(line)
-        return batch.reference
-
     def __gt__(self, other):
         if self.eta is None:
             return False
